5d8fc38d8695f6f500a17505fb007e1115698571
pushmgs <mattsmalleydiscord@gmail.com>
2021-03-08 22:13:51 -0500

    Small historical modification for demonstration

--- simfetch/services/historical.py
+++ simfetch/services/historical.py
@@ -1,32 +1,40 @@
-# Can fetch from either finance API or market data capture.
+from base_pb2 import SecurityBase, CurrencyBase
+from historical_pb2 import InstrumentHistoricalResponse
 
-from generated.base_pb2 import SecurityBase, CurrencyBase
-from generated.historical_pb2 import InstrumentHistoricalResponse
+import historical_pb2_grpc
+
 import json
 
-# Assuming that the security id is already known since we're fetching from a file
-def stream_instrument_json(filename: str) -> InstrumentHistoricalResponse:
-    base = InstrumentHistoricalResponse()
-    with open(filename, 'r') as designated_file:
-        data = designated_file.read()
-    security_data = json.loads(data)
-    for entry in security_data:
-        base.price = entry['price']
-        base.time = entry['time']
-        yield base
+class HistoricalSimulationServicer(historical_pb2_grpc.HistoricalSimulationServicer):
+    # Service to stream historical data back to the codebase so that it can re-evaluate market capture
+    # with newer strategies
 
-# Just returning here instead of having a generator.
-def fetch_instrument_json(filename: str) -> []:
-    responses = []
-    with open(filename, 'r') as designated_file:
-        data = designated_file.read()
-    security_data = json.loads(data)
-    for entry in security_data:
-        base = InstrumentHistoricalResponse()
-        base.price = entry['price']
-        base.time = entry['time']
-        responses.append(base)
-    return responses
+    def __init__(self):
+        pass
+
+    def StreamSecurityHistory(self, request, context) -> InstrumentHistoricalResponse:
+        read_json = True if request.file_path != "" else False
+         # Unsure if causes gRPC clog
+        if not read_json:
+            yield InstrumentHistoricalResponse(price=0, time="DNE")
+
+        # Stream for now
+        instrument_history = _fetch_instrument_history(request.file_path)
+        for value in instrument_history:
+            yield value
+
+    # Just returning here instead of having a generator.
+    def _fetch_instrument_json(filename: str) -> []:
+        responses = []
+        with open(filename, 'r') as designated_file:
+            data = designated_file.read()
+        security_data = json.loads(data)
+        for entry in security_data:
+            base = InstrumentHistoricalResponse()
+            base.price = entry['price']
+            base.time = entry['time']
+            responses.append(base)
+        return responses
 
 # Probably yahoo
 def fetch_api(api_name: str):
